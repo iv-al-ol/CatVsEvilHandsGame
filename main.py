da2a7eab9703aabbb448617ae453525c1650d86b
iv-al-ol <mr.upon@yandex.ru>
2021-03-22 00:15:58 +0300

    Движение квадрата

--- main.py
+++ main.py
@@ -1,43 +1,62 @@
+# Pygame шаблон - скелет для нового проекта Pygame
 import pygame
-import os
 import random
 
-# Основные параметры
-WIDTH = 1600 // 2 # Ширина игрового окна
-HEIGHT = 900 // 2 # Высота игрового окна
-FPS = 30 # Частота кадров в секунду
+WIDTH = 800
+HEIGHT = 650
+FPS = 30
 
-# Цвета (R, G, B)
-BLACK = (0, 0, 0)
+# Задаем цвета
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+        
+        
+    def update(self):
+        self.rect.x += 5
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
+
+
 # Создаем игру и окно
-pygame.init()   # Запускает pygame
-pygame.mixer.init()  # Для звука
-screen = pygame.display.set_mode((WIDTH, HEIGHT))   # Окно программы
-pygame.display.set_caption("Первая тестовая игра")  # Отображение названия в окне
-clock = pygame.time.Clock()     # Убедиться в частоте кадров
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 
 # Цикл игры
 running = True
-
 while running:
-    clock.tick(FPS) # Контроль "тиков" (ФПС)
+    # Держим цикл на правильной скорости
+    clock.tick(FPS)
     # Ввод процесса (события)
-    
     for event in pygame.event.get():
-        # проверить закрытие окна
+        # check for closing window
         if event.type == pygame.QUIT:
             running = False
-    
+
     # Обновление
+    all_sprites.update()
     
-    # Визуализация (сборка)
     # Рендеринг
-    screen.fill(BLACK)  # Залить окно чёрным
-    pygame.display.flip()   # "Повернуть" экран для отображения отрисовок
-    
-pygame.quit # Завершение программы
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+    # После отрисовки всего, переворачиваем экран
+    pygame.display.flip()
+
+pygame.quit()
