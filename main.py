a7d7a406138255e9bdf827eb4b406476f9499243
iv-al-ol <mr.upon@yandex.ru>
2021-04-04 01:25:30 +0300

    Добавлены звуки

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""
+Soundtrack - Fato Shadow
+"""
+
 #====================================================================
 # Импорт необходимых модулей
 #--------------------------------------------------------------------
@@ -12,6 +16,8 @@ WIDTH = 1600
 HEIGHT = 900
 FPS = 60
 
+pg.mixer.init()
+pg.mixer.music.set_volume(0.1)
 pg.display.set_caption("Cat vs Evil Hands")
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 clock = pg.time.Clock()
@@ -19,28 +25,106 @@ clock = pg.time.Clock()
 #====================================================================
 # Работа с файловой системой
 #--------------------------------------------------------------------
-py_folder = os.path.dirname(__file__)   # Папка файла питона
-img_folder = os.path.join(py_folder, 'img') # Папка с изображениями в папке питона
-bullets_folder = os.path.join(img_folder, 'bullets')
-cat_move_folder = os.path.join(img_folder, 'cat_move')
-#--------------------------------------------------------------------
-cat_player_img = pg.image.load(os.path.join(cat_move_folder,
-    'cat_looks_right.png')).convert_alpha()
-#--------------------------------------------------------------------
-evil_hand_img = pg.image.load(os.path.join(img_folder,
-    'evil_hand.png')).convert_alpha()
-#--------------------------------------------------------------------
-background_img = pg.image.load(os.path.join(img_folder,
-    'darkPurple.png')).convert_alpha()
-#--------------------------------------------------------------------
-bullets_img = []
-bullets_list = ['bullet_1.png', 'bullet_2.png','bullet_3.png',
-                 'bullet_4.png', 'bullet_5.png']
-for img in bullets_list:
-    bullets_img.append(pg.image.load(os.path.join(bullets_folder,
-                                                  img)).convert_alpha())
-#--------------------------------------------------------------------
+folder_py       = os.path.dirname(__file__)   # Директория .py файла
 
+folder_img          = 'img'         # Главная папка с изображениями
+folder_cat_move     = 'cat_move'    # Подпапка с изображениями кота
+folder_bullets      = 'bullets'     # Подпапка с изображениями снарядов
+
+folder_snd          = 'snd'         # Главная папка со звуками
+
+#====================================================================
+# Добавление файлов игры
+#--------------------------------------------------------------------
+# Добавление рисунков
+#--------------------------------------------------------------------
+def add_image(add_image_list, folder_img, subfolder_img=None):
+    """Добавляет изображения в игру.
+    
+    add_image_list -> tuple: Имена рисунков с расширениями файлов
+        (например: ['pic_1.png', 'pic_2.png']);
+    folder_img -> str: Имя папки с изображениями;
+    subfolder_img -> str: Имя подпапки с изображениями;
+    
+    Возвращает сконвертированные изображения в составе списка.
+    
+    """
+    folder_img = os.path.join(folder_py, str(folder_img))   # Определение папки с изображениями
+    added_img_list = []
+    if (subfolder_img != None):
+        subfolder_img = os.path.join(folder_img, str(subfolder_img))    # Определение подпапки с изображениями
+        for img in add_image_list:
+            added_img_list.append(pg.image.load(os.path.join(subfolder_img, img)).convert_alpha())
+    else:
+        for img in add_image_list:    
+            added_img_list.append(pg.image.load(os.path.join(folder_img, img)).convert_alpha())
+    return added_img_list
+
+img_cat_player = add_image(['cat_looks_right.png'], folder_img, folder_cat_move)
+img_cat_player = img_cat_player[0]
+
+img_bullets = add_image(['bullet_1.png', 'bullet_2.png',
+                'bullet_3.png', 'bullet_4.png', 'bullet_5.png'], folder_img, folder_bullets)
+
+img_evil_hand = add_image(['evil_hand.png'], folder_img)
+img_evil_hand = img_evil_hand[0]
+
+img_background = add_image(['darkPurple.png'], folder_img)
+img_background = img_background[0]
+
+#--------------------------------------------------------------------
+# Добавление звуков
+#--------------------------------------------------------------------
+def add_sound(add_sound_list, folder_snd, subfolder_snd=None):
+    """Добавляет звуки в игру.
+    
+    add_sound_list -> tuple: Имена звуков с расширениями файлов
+        (например: ['sound_1.mp3', 'sound_2.wav']);
+    folder_snd -> str: Имя папки со звуками;
+    subfolder_snd -> str: Имя подпапки со звуками;
+    
+    Возвращает добавленные звуки в составе списка.
+    
+    """
+    folder_snd = os.path.join(folder_py, str(folder_snd))   # Определение папки со звуками
+    added_sound_list = []
+    if (subfolder_snd != None):
+        subfolder_snd = os.path.join(folder_snd, str(subfolder_snd))    # Определение подпапки со звуками
+        for snd in add_sound_list:
+            added_sound_list.append(pg.mixer.Sound(os.path.join(subfolder_snd, snd)))
+    else:
+        for snd in add_sound_list:    
+            added_sound_list.append(pg.mixer.Sound(os.path.join(folder_snd, snd)))
+    return added_sound_list
+
+def add_music(add_music_list, folder_snd, subfolder_snd=None):
+    """Добавляет музыку в игру.
+    
+    add_music_list -> tuple: Имена музыки с расширениями файлов
+        (например: ['music_1.mp3', 'music_2.wav']);
+    folder_snd -> str: Имя папки c музыкой;
+    subfolder_snd -> str: Имя подпапки с музыкой;
+    
+    Возвращает добавленную музыку в составе списка.
+    
+    """
+    folder_snd = os.path.join(folder_py, str(folder_snd))   # Определение папки с музыкой
+    added_music_list = []
+    if (subfolder_snd != None):
+        subfolder_snd = os.path.join(folder_snd, str(subfolder_snd))    # Определение подпапки с музыкой
+        for mus in add_music_list:
+            added_music_list.append(pg.mixer.music.load(os.path.join(subfolder_snd, mus)))
+    else:
+        for mus in add_music_list:    
+            added_music_list.append(pg.mixer.music.load(os.path.join(folder_snd, mus)))
+    return added_music_list
+
+soundtrack_1 = add_music(['soundtrack_1.mp3'], folder_snd)
+soundtrack_1 = soundtrack_1[0]
+
+snd_shoot = add_sound(['shoot_1.wav', 'shoot_2.wav', 'shoot_3.wav'], folder_snd)
+
+snd_explosion = add_sound(['explosion_1.wav', 'explosion_2.wav', 'explosion_3.wav'], folder_snd)
 #====================================================================
 # Задание цветов
 #--------------------------------------------------------------------
@@ -53,30 +137,44 @@ YELLOW        = (255, 255,   0)
 JADE          = (  0, 168, 107)
 DARK_BROWN    = (101,  67,  33)
 
+
 #####################################################################
 # Функции
 #####################################################################
 def draw_ground():
-    """
-    Закрашивает фон экрана рисунком
-    """
-    background_rect = background_img.get_rect()
+    """Закрашивает фон экрана рисунком"""
+    background_rect = img_background.get_rect()
     
     ground_limit_x = WIDTH // background_rect.width + 1
     ground_limit_y = HEIGHT // background_rect.height + 1
     
     for line_y in range (ground_limit_y):
         for line_x in range(ground_limit_x):
-            screen.blit(background_img, (0 + line_x*(background_rect.height),
+            screen.blit(img_background, (0 + line_x*(background_rect.height),
                                          0 + line_y*(background_rect.height)))
 
 def debug_hits(self):
-    """
-    Рисование круга для проверки столкновений
-    """
+    """Рисует круг для проверки столкновений"""
     self.rect = self.image.get_rect()
     self.radius = self.image.get_height() // 2 - int(self.image.get_height() * 0.1)
     pg.draw.circle(self.image, RED, self.rect.center, self.radius)
+
+def draw_text(surf, text, size, x, y):
+    """Выводит текст на поверхности с заданными параметрами.
+    
+    surf: Поверхность отображения текста;
+    text: Отображаемый текст;
+    size: Размер шрифта;
+    x: Координата середины блока текста по x 
+    y: Координата верха блока текста по y
+    
+    """
+    font_name = pg.font.match_font('arial')
+    font = pg.font.Font(font_name, size)
+    text_surface = font.render(text, True, WHITE)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surf.blit(text_surface, text_rect)
 
 #####################################################################
 # ОБЪЕКТЫ
@@ -85,20 +183,19 @@ def debug_hits(self):
 # Объект игрока
 #--------------------------------------------------------------------
 class Cat(pg.sprite.Sprite):
+    """Объект игрока. Выводит спрайт кота."""
+    
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
-        self.image = pg.transform.scale(cat_player_img,
-                                        (cat_player_img.get_width()*2,
-                                         cat_player_img.get_height()*2))       
+        self.image = pg.transform.scale(img_cat_player,
+                                        (img_cat_player.get_width()*2,
+                                         img_cat_player.get_height()*2))       
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH // 2
         self.rect.centery = HEIGHT // 2
 
         self.speed_x = 0
         self.speed_y = 0
-        
-        self.x_touch = 0    # Касание по х
-        self.y_touch = 0    # Касание по y
         
     def update(self):
         self.speed_x = 0
@@ -131,19 +228,27 @@ class Cat(pg.sprite.Sprite):
         if self.rect.top < 0:
             self.rect.top = 0
         
-    def shot(self, shot_direction):
+    def shoot(self, shot_direction):
+        """Выводит спрайт снаряда.
+        
+        shot_direction: направление полета снаряда.
+        Принимает значения типа str(): 'up', 'down', 'left', 'right'.
+        
+        """
         bullet = Bullet(self.rect.right + 10, self.rect.centery - 10, shot_direction)
         all_sprites.add(bullet)
         bullets.add(bullet)
+        rnd.choice(snd_shoot).play()
+        
         
 #====================================================================
 # Объект противника
 #--------------------------------------------------------------------
 class EvilHand(pg.sprite.Sprite):
+    """Объект противника. Выводит спрайт злых рук."""
+    
     def evil_hand_pos(self):
-        """
-        Определяет начальную позицию и скорость
-        """
+        """Определяет начальную позицию и скорость злых рук."""
         self_rect_x = rnd.randrange(3*(-self.rect.width),
                                     WIDTH + 3*(self.rect.width))
         if self_rect_x > WIDTH // 2:
@@ -169,7 +274,7 @@ class EvilHand(pg.sprite.Sprite):
     
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
-        self.image_orig = evil_hand_img
+        self.image_orig = img_evil_hand
         self.image = self.image_orig.copy()
         self.rect = self.image.get_rect()
         EvilHand.evil_hand_pos(self)
@@ -209,13 +314,16 @@ class EvilHand(pg.sprite.Sprite):
         if self.rect.bottom < 0 - 3*self.rect.height:
             EvilHand.evil_hand_pos(self)
         
+        
 #====================================================================
 # Объект выстрелов
 #--------------------------------------------------------------------
 class Bullet(pg.sprite.Sprite):
+    """Объект снарядов. Выводит спрайт снарядов."""
+    
     def __init__(self, x, y, shot_direction):
         pg.sprite.Sprite.__init__(self)
-        self.image = rnd.choice(bullets_img)
+        self.image = rnd.choice(img_bullets)
         self.rect = self.image.get_rect()
 
         self.rect.centerx = x
@@ -248,11 +356,11 @@ class Bullet(pg.sprite.Sprite):
         if self.rect.left > WIDTH:
             self.kill()
 
+
 #====================================================================
-# Создаем игру и окно
+# Создаем окно игры
 #--------------------------------------------------------------------
 pg.init()
-pg.mixer.init()
 #--------------------------------------------------------------------
 
 #====================================================================
@@ -276,8 +384,15 @@ for i in range(rnd.randrange(15, 30)):
 #--------------------------------------------------------------------
 
 #====================================================================
+# Подсчет очков
+#--------------------------------------------------------------------
+score = 0
+
+#====================================================================
 # Цикл игры
 #--------------------------------------------------------------------
+pg.mixer.music.play(loops=-1)   # Запуск музыки
+
 running = True
 while running:
     clock.tick(FPS) # Держим цикл на правильной скорости
@@ -291,16 +406,16 @@ while running:
         elif event.type == pg.KEYDOWN:
             if (event.key == pg.K_UP):
                 shot_direction = 'up'
-                cat_player.shot(shot_direction)
+                cat_player.shoot(shot_direction)
             elif (event.key == pg.K_DOWN):
                 shot_direction = 'down'
-                cat_player.shot(shot_direction)
+                cat_player.shoot(shot_direction)
             elif (event.key == pg.K_LEFT):
                 shot_direction = 'left'
-                cat_player.shot(shot_direction)
+                cat_player.shoot(shot_direction)
             elif (event.key == pg.K_RIGHT):
                 shot_direction = 'right'
-                cat_player.shot(shot_direction)
+                cat_player.shoot(shot_direction)
             else:
                 shot_direction = None
     #----------------------------------------------------------------
@@ -313,6 +428,8 @@ while running:
     hits = pg.sprite.groupcollide(bullets, mobs, True, True,
                                   pg.sprite.collide_rect_ratio(0.95))
     for hit in hits:
+        score += 1
+        rnd.choice(snd_explosion).play()
         eh = EvilHand()
         all_sprites.add(eh)
         mobs.add(eh)
@@ -327,8 +444,9 @@ while running:
     
     #----------------------------------------------------------------
     draw_ground()   # Закрасить фон
-    
     all_sprites.draw(screen)    # Отрисовка всех спрайтов
+    draw_text(screen, str('Уничтожено рук: %s' % score), 20, WIDTH // 2, 10)
+#-------------------------------------------------------------------- 
     pg.display.flip()   # После отрисовки всего, переворачиваем экран
 #--------------------------------------------------------------------
 
